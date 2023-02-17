04e92aae7c44f7b59d013e2b72fa0d2e5fb6725c
Luke Davis <luke.davis.16@ucl.ac.uk>
2023-02-17 19:48:14 +0000

    Add files via upload

---
+++ make_pairstyle_example_WCA.py
@@ -0,0 +1,91 @@
+
+
+## Written by Dr. Luke Davis: UCL Department of Mathematics 2023 luke.davis@ucl.ac.uk
+
+##
+
+##
+
+#!/usr/bin/env python
+
+# coding: utf-8
+
+
+
+import warnings
+import os
+import math
+import sys # python system library
+import numpy as np # numerical python library
+import math # python math library
+import scipy # scipy is nearly always useful...
+from functools import partial # to pass functions into functions...
+import create_lammps_pairstyle_table as clpt # functions to create and test tabulated pair potential
+
+
+# add your pairstyles in potentials_for_lammps.py and import it like below :)
+
+from potentials_for_lammps import pair_WCA
+warnings.simplefilter('ignore')
+
+
+
+# lammps units
+units_string = "lj"
+
+# particular arguments for the pairstyle
+eps = 10.0
+sigma = 3.0
+rc = 2**(1.0/6.0)*sigma
+# particular options for the table
+
+rmin = 0.5 # minimum distance
+rmax = rc+sigma # maximum distance
+N = 2000 # number of distance values between rmin and rmax to use
+rdelta = (rmax-rmin)/float(N)
+rlist = np.arange(rmin,rmax,rdelta)
+
+# define a cut-off due to tabulation
+rcdiffp = float('inf')
+rctab = float('inf')
+for r in rlist:
+    if r < rc:
+        rcdiff = rc - r
+        if rcdiff < rcdiffp:
+            rcdiffp = rcdiff
+            rctab = r
+
+# make the table
+
+pair_filename = "./Example_pair_WCA.txt"
+pair_keyword = "WCA"
+clpt.make_table_for_lammps(pair_filename,pair_keyword,pair_WCA, rmin, rmax, N,rctab,eps,sigma,rc)
+
+
+# put the table through lammps and check it
+lmps_input_filename = "./in.Example_lmps_pair_WCA"
+lmps_pair_filename = "./Example_lmps_pair_WCA.txt"
+lmps_executing_command = "/clusternfs/ldavis/lammps/lammps-29Sep2021/src/lmp_serial -i" # change so it points to your lammps exe
+
+
+
+
+# get lammps to pair_write the potential energy and force for comparison
+
+Nlmps = N
+clpt.pair_write_lammps(lmps_input_filename,lmps_pair_filename,lmps_executing_command
+
+                       ,pair_filename, pair_keyword
+
+                       ,units_string,rmin,rmax,N+1,Nlmps,rc,style='linear')
+
+
+
+# Compare the data in files visually
+
+clpt.comparison("Example_pair_WCA",lmps_pair_filename,pair_filename,rmin,rmax,rdelta,-2,20,-2,20,plot=True,markers=False) 
+
+
+
+# [ for the above clpt.comparison()] turn plot=True to plot=False to only output file for relative differences between lammps and your original table. In the above test_pair_LJ_rel_differences.txt contains all the numrical difference data and test_pair_LJ.pdf is the plot of the potentials and forces. So the first argument is <some-string> which will make <some-string>_rel_differences.txt and <some-string>.pdf
+
---
+++ potentials_for_lammps.py
@@ -0,0 +1,132 @@
+## Written by Dr. Luke Davis: UCL Department of Mathematics 2023 luke.davis@ucl.ac.uk
+##
+##
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import autograd.numpy as np # numerical python library
+import math # python math library
+from autograd import grad # automatic differentiation (to get the force)
+from functools import partial # to pass functions into functions...
+
+### Generic force function ###
+def force(pair,*args):
+    f =  grad(pair)
+    return -f(*args)
+
+### Pair Potentials ###
+def pair_debug(r):
+    return r
+
+# Example Lennard Jones
+def pair_LJ(r,eps,sigma,rc):
+    ene = 0.0
+
+    # Impose a cutoff
+    if r < rc:
+        ene += 4.0*eps*((sigma/r)**12 - (sigma/r)**6)
+
+    # A pair function should only return the potential energy at r
+    return ene
+
+
+# Example WCA  potential
+def pair_WCA(r,eps,sigma,rc):
+    ene = 0.0
+
+    # Impose a cutoff
+    if r < rc:
+        ene += 4.0*eps*((sigma/r)**12 - (sigma/r)**6)+eps
+
+    # A pair function should only return the potential energy at r
+    return ene
+
+# A more involved example
+def pair_1(r,typei,typej,intmatrix,
+         eps_excl,sigma, # for the excluded volume
+         eps_coh,rcoh, # for the cohesion
+         Qi,Qj,dieconst,kappa,rcoul, # for the electrostatics
+         ):
+
+    # determine what interactions should be included for i and j
+    excl_on = intmatrix[typei-1][typej-1][0]
+    coh_on = intmatrix[typei-1][typej-1][1]
+    coul_on = intmatrix[typei-1][typej-1][2]
+
+    # initialise enes
+    excl_ene = 0.0
+    coh_ene = 0.0
+    coul_ene = 0.0
+
+    # setup somethings
+    epsconvfact = (1. + 4.*((sigma/rcoh)**12 - (sigma/rcoh)**6 ) + (2.**(1/6)*sigma - rcoh)*4.*(6.*((sigma**6)/(rcoh**7))-12.*((sigma**12)/(rcoh**13))))
+    epsconvfact = 1./epsconvfact
+    eps_conv = eps_coh*epsconvfact
+
+    rcexcl = 2.0**(1./6.)*sigma
+
+    # add contribs to ene
+
+    ## Volume exclusion
+    if r < rcexcl and excl_on:
+        excl_ene += 4.0*eps_excl*((sigma/r)**12 - (sigma/r)**6)+eps_excl
+
+    ## Cohesion
+    if r < rcoh and coh_on:
+        coh_ene += 4.0*eps_conv*((sigma/r)**12 - (sigma/r)**6)
+        coh_ene += -4.0*eps_conv*((sigma/rcoh)**12 - (sigma/rcoh)**6)
+        coh_ene += -(r-rcoh)*4.0*eps_conv*( ((6.0*sigma**6)/(rcoh**7)) - ((12.0*sigma**12)/(rcoh**13)))
+
+    ## electrostatics
+    if r < rcoul and coul_on:
+        coul_ene += (56.11 * Qi * Qj)/(dieconst*r)*math.exp(-kappa*r) # note ke * e^2 * (1/(4.11x10^-21)) * 10^9 = 56.11 (see notes)
+
+    return excl_ene + coh_ene + coul_ene
+
+
+## Potential to control repulsion and cohesive tail independently whilst preserving smoothness
+def ELJcohesion(r, epc,sigma, rc):
+    rcrep =2.0**(1./6.)*sigma
+    if r < rcrep:
+            
+        return 0.0
+            
+    elif r <= rc:
+            
+        fbegin = force(partial(pair_LJ),rcrep,epc,sigma,rc)
+        fend = force(partial(pair_LJ),rc,epc,sigma,rc)
+            
+        #return pair_LJ(r,epc,sigma,rc)-pair_LJ(rc,epc,sigma,rc) + (r-rc)*fend + (r-rcrep)*fbegin
+        return pair_LJ(r,epc,sigma,rc)-pair_LJ(rc,epc,sigma,rc) + (r-rcrep)*fbegin
+            
+    else:
+            
+        return 0.0
+            
+def ELJrepulsion(r, epr, epc, sigma, rc):
+    rcrep =2.0**(1./6.)*sigma
+    if r <= rcrep:
+            
+        fendrep = force(partial(pair_LJ),rcrep,epr,sigma,rcrep)
+        return pair_LJ(r,epr,sigma,rcrep) + ELJcohesion(rcrep,epc,sigma,rc) + epr + (r-rcrep)*fendrep
+
+    if r > rcrep:
+
+        return 0.0
+
+
+def pair_rep_coh(r,epr,epc,sigma,rc,rctab):
+    
+    if r <= rc:
+        return ELJcohesion(r,epc, sigma, rc) + ELJrepulsion(r,epr, epc, sigma, rc)
+    else:
+        return 0.0
+
+# Version which is shifted and continuous at the tabulated cut-off
+def pair_rep_coh_smooth_linear(r,epr,epc,sigma,rc,rctab):
+    if r <= rc:
+        fend = force(partial(pair_rep_coh),rctab,epr,epc,sigma,rc,rctab)
+        return pair_rep_coh(r,epr,epc,sigma,rc,rctab) - pair_rep_coh(rctab,epr,epc,sigma,rc,rctab) + (r-rctab)*fend
+    else:
+        return 0.0
